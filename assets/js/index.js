0d3ea7b67d54bd41efbbaddeeaf4e9c6bff302f6
tranhung18 <hungth@regit-technology.com>
2023-06-07 16:49:36 +0700

    Fix Bug Design

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,5 +3,14 @@ function menuMobile(value){
 }
 function searchMobile(value){
     document.querySelector('.search-mobile').style.width = `${value}%`;
-
 }
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.container-header');
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+    if (scrollTop > 0) {
+      header.classList.add('header-scrolled');
+    } else {
+      header.classList.remove('header-scrolled');
+    }
+  });
